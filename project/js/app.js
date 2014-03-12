bef487d4c68789592663de25130158fa2f7e2bb3
Kenzan Hase <kenzan8000@gmail.com>
2014-03-12 11:06:20 +0800

    fix app icons and add adding pin function

--- project/js/app.js
+++ project/js/app.js
@@ -1,7 +1,39 @@
+/**
+ * Livedoor ReaderのPINにリンクを追加
+ * @param link linkのURL
+ * @param titlte linkのタイトル
+ * @return jQuery.Deferred
+ */
+var addPin = function(link, title) {
+    // API
+    var APIURL = "http://reader.livedoor.com/api/pin/add?link=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title);
+
+    // ajax
+    var deferred = jQuery.Deferred();
+    jQuery.ajax({
+        type: "POST",
+        url: APIURL,
+        success: deferred.resolve,
+        error: deferred.reject
+    });
+    return deferred.promise();
+};
+
+/**
+ * ナビゲーションバーのボタンを押下
+ */
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(null, {
-            "code": "alert('" + tabs[0].url + "')"
-        });
+        // PIN追加
+        var link = tabs[0].url;
+        var title = tabs[0].title;
+        addPin(link, title)
+            .fail(function() {
+                // Livedoor Readerのログイン画面を開く
+                //chrome.tabs.create({url: "https://member.livedoor.com/login/"});
+            })
+            .done(function() {
+            });
     });
 });
+
