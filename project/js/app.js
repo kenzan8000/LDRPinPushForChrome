2634210002c481701b1dc78f9f8735a6eb7e8e7f
Kenzan Hase <kenzan8000@gmail.com>
2014-03-13 10:51:01 +0800

    add pin push success and failure notification

--- project/js/app.js
+++ project/js/app.js
@@ -13,8 +13,18 @@ var addPin = function(link, title) {
     jQuery.ajax({
         type: "POST",
         url: APIURL,
-        success: deferred.resolve,
-        error: deferred.reject
+        success: function(data, status, xhr) {
+            var statusCode = xhr.status;
+            if (xhr.responseText == "Not Authorized") { statusCode = 401; }
+            else if (xhr.responseText["ErrorCode"] == 401) { statusCode = 401; }
+            (statusCode == 401) ? deferred.reject() : deferred.resolve();
+        },
+        error: function(xhr, exception) {
+            var statusCode = xhr.status;
+            if (xhr.responseText == "Not Authorized") { statusCode = 401; }
+            else if (xhr.responseText["ErrorCode"] == 401) { statusCode = 401; }
+            (statusCode == 401) ? deferred.reject() : deferred.resolve();
+        }
     });
     return deferred.promise();
 };
@@ -30,10 +40,17 @@ chrome.browserAction.onClicked.addListener(function() {
         addPin(link, title)
             .fail(function() {
                 // Livedoor Readerのログイン画面を開く
-                //chrome.tabs.create({url: "https://member.livedoor.com/login/"});
+                chrome.tabs.create({url: "https://member.livedoor.com/login/"});
+                // notification
+                chrome.notifications.create(title,
+                                            { type: "basic", title: "ピンの追加に失敗しました", message: "もう一度、ログインし直してください", iconUrl:"icons/icon48.png" },
+                                            function(id) { });
             })
             .done(function() {
+                // notification
+                chrome.notifications.create(title,
+                                            { type: "basic", title: "ピンを追加しました", message: title + "\n" + link, iconUrl:"icons/icon48.png" },
+                                            function(id) { });
             });
     });
 });
-
